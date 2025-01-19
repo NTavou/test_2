7dafeae323f349e35e390846915bb7a12443ab23
Nikos Tavoularis <nikolaos.tavoularis@gmail.com>
2025-01-20 01:06:05 +0200

    second commit

--- app.py
+++ app.py
@@ -8,21 +8,22 @@ import pandas as pd
 from datetime import datetime
 
 
-# Specify the file path
-file_path = r'C:\\Users\\nikos\\basketball_forecasting-master\\2024_2025\\01_Todays_Matches.xlsx'  # Update this path to match your file's location
-
 # Import the Excel file
-data = pd.read_excel(file_path)
+data = pd.read_excel('01_Todays_Matches_final.xlsx')
 
 # Display the date of the bet forecasts
-day = data.iloc[0,1]
-month = data.iloc[0,2]
-year = data.iloc[0,3]
+day = data.iloc[0,0]
+month = data.iloc[0,1]
+year = data.iloc[0,2]
 
 
 title = f'Bets for {day}-{month}-{year}:'
 
 title
 
-# Display the first few rows of the dataframe
-data.iloc[:, [1, 2, 3, 4, 5,  6, 8, 9]]
+data
+
+
+
+
+
